8ef0c8b0b1c8131e50988d50984ed3ff170e66b4
MisterSigal <qivey@bk.ru>
2019-10-14 16:41:50 +0300

    Update example.js

--- example.js
+++ example.js
@@ -2,11 +2,17 @@ const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const firefox = require('selenium-webdriver/firefox');
 
-let driver = new webdriver.Builder()
-	.withCapabilities({'browserName': 'chrome', 'enableVNC': true })
+(async function example() {
+  let driver = await new Builder()
+    .withCapabilities({'browserName': 'chrome', 'enableVNC': true })
     .usingServer('http://0.0.0.0:4444/wd/hub')	
     .build();
-
-driver.get('https://google.com');
-console.log(driver);
-driver.quit();
+  try {
+    await driver.get('http://www.google.com/ncr');
+    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  } finally {
+    await console.log(driver);
+    await driver.quit();
+  }
+})();
