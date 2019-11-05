ee32f1a89789c51aee28371f413c252f259a676e
MisterSigal <qivey@bk.ru>
2019-11-05 13:31:23 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,9 @@ apt-get install nodejs -y
 curl -s https://aerokube.com/cm/bash | bash
 ./cm selenoid start --vnc | bash
 ./cm selenoid-ui start
+echo "Selenoid is ready!"
 docker run -p 5601:5601 -p 9200:9200 -p 5044:5044 -it --name elk sebp/elk
-echo "EVOQA is Ready!"
+echo "ELK is ready!"
+npm run
+echo "Selenium is built"
+echo "EVOQA is ready to work!"
