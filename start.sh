b82ba0def284c3fc7fc1668f0844941dbb8ff892
MisterSigal <qivey@bk.ru>
2019-11-06 09:42:46 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,7 +17,7 @@ curl -s https://aerokube.com/cm/bash | bash
 echo "Selenoid is ready!"
 
   #elk
-docker run -p 5601:5601 -p 9200:9200 -p 5044:5044 -it --name elk sebp/elk
+docker run -d -p 5601:5601 -p 9200:9200 -p 5044:5044 -it --name elk sebp/elk
 echo "ELK is ready!"
 
 #selenium
