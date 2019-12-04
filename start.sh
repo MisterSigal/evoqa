a886fe545de552d5897e3397a5a08cc0a9a75180
al <e.vojnovskij@samoletgroup.ru>
2019-12-04 13:53:10 +0300

    gitignore pravka

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ apt-get update
 apt-get install npm -y
 apt-get install nodejs -y
 apt-get install openjdk-8-jdk-headless -y
-docker-compose installation
+# docker-compose installation
 # sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # sudo chmod a+rx /usr/local/bin/docker-compose  
 
