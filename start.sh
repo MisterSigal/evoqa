3c9d0fedee881533e6ae9e52e3042e4db11fdb3b
al <e.vojnovskij@samoletgroup.ru>
2019-12-04 13:23:45 +0300

    ELK stack refactor

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-#apps installation
+# apps installation
 apt-get update
 apt-get install npm -y
 apt-get install nodejs -y
 apt-get install openjdk-8-jdk-headless -y
+docker-compose installation
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod a+rx /usr/local/bin/docker-compose  
 
 #Machine tweaks
 sudo sysctl -w vm.max_map_count=262145
@@ -17,7 +20,8 @@ curl -s https://aerokube.com/cm/bash | bash
 echo "Selenoid is ready!"
 
   #elk
-docker run -d -p 5601:5601 -p 9200:9200 -p 5044:5044 -it --name elk sebp/elk
+git clone https://github.com/MisterSigal/docker-elk.git
+docker-compose -f ./docker-elk/docker-compose.yml up -d
 echo "ELK is ready!"
 
 #selenium
