732d8a6e851eeeeffad791ebedc1d53d1cdbd471
MisterSigal <qivey@bk.ru>
2020-03-19 10:17:55 +0300

    Update base.js

--- selenium/test/base.js
+++ selenium/test/base.js
@@ -1,45 +1,52 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
-const chromedriver = require('chromedriver').path;
-
-//Page Object imports
-const login = require('./pageobject/login');
-const user_tn = require('./pageobject/users/users');
-const mainPage = require('./pageobject/mainpage');
-
-const localHost = 'http://0.0.0.0:8091/';
-const remoteHost = 'http://0.0.0.0:4444/wd/hub';
-
+const {
+    Builder,
+    By,
+    Key,
+    until
+} = require('selenium-webdriver');
+const fs = require('fs');
 class Main {
-  constructor(driver){
-    this.driver = driver;
-  }
+    constructor(driver) {
+        this.driver = driver;
+    }
+    startLocalBrowser(browserName) {
+        return this.driver = new Builder().forBrowser(browserName).build()
+    }
+    startRemoteBrowser(seleniumServer, browserName) {
+        return this.driver = new Builder().withCapabilities({
+            'browserName': browserName,
+            'enableVNC': true
+        }).usingServer(seleniumServer).build()
+    }
+    get(url) {
+        return this.driver.get(url)
+    }
+    waitFor(selector, element, timeout) {
+        return this.driver.wait(until.elementLocated(selector(element)), timeout);
+    }
+    findElement(selector, element) {
+        return this.driver.findElement(selector(element));
+    }
+    screenShot(path, title) {
+        return this.driver.takeScreenshot().then(function(data) {
+            var base64Data = data.replace(/^data:image\/png;base64,/, "") fs.writeFile(__dirname + path + title + ".png", base64Data, 'base64', function(err) {
+                if (err) console.log(err);
+            });
+        });
+    }
+    sleep(ms) {
+        return this.driver.sleep(ms);
+    }
+    script(script) {
+        return this.driver.executeScript(script);
+    }
+    quit() {
+        return this.driver.quit();
+    }
 }
-
-
-const remoteChrome = new Builder()
-  .withCapabilities({'browserName': 'chrome', 'enableVNC': true })
-  .usingServer(remoteHost);
-
- const localChrome = new Builder()
-   .withCapabilities(new chrome.ServiceBuilder(__dirname + '/../drivers/chromedriver'))
-   .forBrowser('chrome');
-
-
-async function testCase() {
-  const t = await new Main(localChrome, localHost);
-  const b = await t.driver.build();
-  await console.log('in test case');
-  await b.get(localHost);
-  await b.wait(until.elementLocated(By.id(login.username)));
-  await b.findElement(By.id(login.username)).sendKeys(user_tn.name);
-  await b.findElement(By.id(login.password)).sendKeys(user_tn.password);
-  await b.findElement(By.className(login.loginButton)).click();
-  await b.wait(until.elementLocated(By.className(mainPage.createIssue)));
-  await b.findElement(By.className(mainPage.createIssue)).click();
-  await b.findElement(By.className('sc-jAaTju PykSP ant-select ant-select-enabled ant-select-allow-clear')).click();
-  await console.log('test case end');
+module.exports = {
+    Main,
+    By,
+    Key,
+    until
 }
-testCase();
-
-
